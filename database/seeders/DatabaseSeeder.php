8a70566f59eb1b1a1e735e7a3f01bc9cff3c6ede
Alessio Cela <alessiocela02@gmail.com>
2024-05-27 12:08:47 +0200

    Update DatabaseSeeder.php
    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Provider\Lorem;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+        DB::table("account")->insert([
+            ['id'=>1,'username'=>'paziente1', 'pwd' => Hash::make('test'), 'nome'=>'mario', 'cognome'=>'ciao', 'dataDiNascita'=>'2000-05-12', '1'],
+            ['id'=>2,'username'=>'clinico1', 'pwd' => Hash::make('test2'), 'nome'=>'mario2', 'cognome'=>'ciao2', 'dataDiNascita'=>'2000-04-12', '2'],
+            ['id'=>3,'username'=>'admin1', 'pwd' => Hash::make('test'), 'nome'=>'mario', 'cognome'=>'ciao', 'dataDiNascita'=>'2000-05-12', '1']
+        ]);
+        DB::table('clinico')->insert([['idAccount'=>2,'specializzazione'=>'spec1prova','ruolo'=>'medico']]);
+        DB::table('paziente')->insert([['idAccount'=> 1,'genere'=>'maschio', 'indirizzo'=> 'via ciao 3','telefono'=> '3279453367', 'email'=> 'ciao@ciaociao.it','idClinico'=>2]]);
+        DB::table('faq')->insert([['domanda'=> "come funziona il sito?",'risposta'=>'loremipsum loremipsum loremipsum loremipsum loremipsum loremipsum ']]);
+        DB::table('listaFarmaci')->insert([['nome'=>'test','descrizione'=> 'testtestest']]);
+        DB::table('listaTerapieRiabilitative')->insert([['nome'=> 'test', 'descrizione'=> 'testtestest']]);
+        DB::table('listaDisturbiMotori')->insert([['nome'=> 'test', 'descrizione'=> 'testtestest']]);
+}
 }
