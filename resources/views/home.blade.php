a4d81763d504aedaf673b98e6017b6f685fcffcf
otech99 <otechmr@gmail.com>
2024-05-28 16:55:29 +0200

    prova

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <h3>This is Home page</h3>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt magni deleniti modi dolores, ipsa, eveniet ex amet dicta porro quaerat nemo excepturi a quos, numquam tempora sequi id cum aut.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit repellendus doloribus temporibus perspiciatis ab numquam! Dolorum aliquid, porro officiis accusantium molestiae repellendus assumenda corporis ducimus? Molestias ex accusamus dolorem assumenda!</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi ratione alias error similique. Consequuntur fuga ut et? Assumenda maxime reiciendis adipisci architecto pariatur. Quae quas voluptates qui consequuntur architecto natus.</p>
+    <p>Lorem ipsum, dolor sitmm amet consectetur adipisicing elit. Nisi ratione alias error similique. Consequuntur fuga ut et? Assumenda maxime reiciendis adipisci architecto pariatur. Quae quas voluptates qui consequuntur architecto natus.</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis illum ullam expedita sed, non quis sunt laborum debitis possimus modi labore nulla dolores alias beatae dolor nam eius commodi ea?</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, placeat? Rem eaque provident illum libero, ipsum at expedita eveniet, placeat vel soluta maiores. Est, eius provident praesentium recusandae molestias necessitatibus!</p>
   </main>
